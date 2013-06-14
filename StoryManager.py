8699d630fbfbecba1dcb96accad7d4e1d9a68bbe
Simon Townsend <stownsend@iseatz.com>
2013-06-14 01:06:13 -0500

    support for columns or 'groups'

--- StoryManager.py
+++ StoryManager.py
@@ -57,10 +57,10 @@ class OpenStoryCommand(sublime_plugin.WindowCommand):
         stories_directory = os.listdir(self.STORIES_PATH)
         for file_path in stories_directory:
             if not file_path.startswith('.'): # ignore . system files (.DS_Store)
-                with open(self.STORIES_PATH + '/' + file_path, 'rb') as story_path:
-                    story_json = json.load(story_path)
+                with open(self.STORIES_PATH + '/' + file_path, 'rb') as story_file:
+                    story_json = json.load(story_file)
 
-                    self.story_paths.append(self.STORIES_PATH + '/' + story_path)
+                    self.story_paths.append(self.STORIES_PATH + '/' + file_path)
                     self.story_names.append(story_json['title'])  # used for the dropdown display name
         
         self.window.show_quick_panel(self.story_names, self.story_selected)
@@ -76,11 +76,20 @@ class OpenStoryCommand(sublime_plugin.WindowCommand):
             
             with open(self.story_path, 'rb') as json_file:
                 json_data = json.load(json_file)
-                set_layout(w, len(json_data['groups']))
-                #for 
-                #self.window.open_file(row[0])
 
-    def set_layout(w, num_columns):
+                # create the view groups
+                num_groups = len(json_data['groups'])
+                if num_groups > 1:
+                    self.set_layout(num_groups)
+                    #iterate the groups
+                    for i in range(num_groups):
+                        w.run_command('focus_group', { 'group' : i })
+                        #iterate and open the files in the group
+                        for index, j in enumerate(json_data['groups'][i]):
+                            view = self.window.open_file(j)
+                            w.set_view_index(view, w.active_group(), index)
+
+    def set_layout(self, num_columns):
         layouts = [{
                         "cols": [0.0, 0.5, 1.0],
                         "rows": [0.0, 1.0],
@@ -97,6 +106,6 @@ class OpenStoryCommand(sublime_plugin.WindowCommand):
                         "cells": [[0, 0, 1, 1], [1, 0, 2, 1], [2, 0, 3, 1], [3, 0, 4, 1]]
                     }
                   ]
-        w.run_command('set_layout', layouts[num_columns - 2])
+        self.window.run_command('set_layout', layouts[num_columns - 2])
 
 
